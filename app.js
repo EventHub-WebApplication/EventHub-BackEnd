774ebedcac9bb048b2f45d62107476d767fab5c4
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2022-01-19 21:59:56 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,182 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app =express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://saruj:Jom_12345@cluster0.f0hxk.mongodb.net/EventHub", {useNewUrlParser: true});
+
+const eventSchema = {
+    eventId: Number,
+    eventName: String,
+    description: String,
+    amount: Number,
+    owner: Number,
+    categories: [String],
+    paticipant: [Number]
+};
+
+const Event = mongoose.model("Event", eventSchema);
+
+app.route("/events")
+
+.get(function(req, res){
+    Event.find(function(err, events){
+      if (events) {
+        const jsonEvents = JSON.stringify(events);
+        res.send(jsonEvents);
+      } 
+      else {
+        res.send("Database is empty");
+      }
+    });
+  })
+  
+.post(function(req, res){
+    const newEvent = Event({
+        eventId: req.body.eventId,
+        eventName: req.body.eventName,
+        description: req.body.description,
+        amount: req.body.amount,
+        owner: req.body.owner,
+        categories: req.body.categories,
+        paticipant: req.body.paticipant
+    });
+  
+    newEvent.save(function(err){
+      if (!err){
+        res.send("Successful");
+      } 
+      else {
+        res.send(err);
+      }
+    });
+  })
+
+app.route("/events/:eventId")
+
+.get(function(req, res){
+  const eventId = req.params.eventId;
+  Event.findOne({eventId: eventId}, function(err, event){
+    if (event){
+      const jsonEvent = JSON.stringify(event);
+      res.send(jsonEvent);
+    } 
+    else {
+      res.send("Event not exist");
+    }
+  });
+})
+
+.patch(function(req, res){
+    const eventId = req.params.eventId;
+    Event.update(
+      {eventId: eventId},
+      {$set: req.body},
+      function(err){
+        if (!err){
+          res.send("updated");
+        } 
+        else {
+          res.send(err);
+        }
+      });
+  })
+
+.delete(function(req, res){
+    const eventId = req.params.eventId;
+    Event.findOneAndDelete({eventId: eventId}, function(err){
+      if (!err){
+        res.send("Deleted");
+      } 
+      else {
+        res.send(err);
+      }
+    });
+  });
+
+app.route("/category/:category")
+
+.get(function(req, res){
+    const category = req.params.category;
+    Event.find({categories: category}, function(err, event){
+      if (event){
+        const jsonEvent = JSON.stringify(event);
+        res.send(jsonEvent);
+      } 
+      else {
+        res.send("No event in this category");
+      }
+    });
+  })
+
+app.route("/ownedEvent/:userId")
+
+.get(function(req, res){
+    const user = req.params.userId;
+    Event.find({owner: user}, function(err, event){
+      if (event){
+        const jsonEvent = JSON.stringify(event);
+        res.send(jsonEvent);
+      } 
+      else {
+        res.send("No event found");
+      }
+    });
+  })
+
+app.route("/paticipatedEvent/:userId")
+
+.get(function(req, res){
+    const user = req.params.userId;
+    Event.find({paticipant: user}, function(err, event){
+      if (event){
+        const jsonEvent = JSON.stringify(event);
+        res.send(jsonEvent);
+      } 
+      else {
+        res.send("No event found");
+      }
+    });
+  })
+
+app.route("/events/:eventId/join/:userId")
+
+.patch(function(req, res){
+    const eventId = req.params.eventId;
+    const userId = req.params.userId;
+    Event.update(
+      {eventId: eventId},
+      {$push: {paticipant: userId}},
+      function(err){
+        if (!err){
+          res.send("join successful");
+        } 
+        else {
+          res.send(err);
+        }
+      });
+  })
+
+app.route("/events/:eventId/cancel/:userId")
+
+  .patch(function(req, res){
+      const eventId = req.params.eventId;
+      const userId = req.params.userId;
+      Event.update(
+        {eventId: eventId},
+        {$pull: {paticipant: userId}},
+        function(err){
+          if (!err){
+            res.send("cancel successful");
+          } 
+          else {
+            res.send(err);
+          }
+        });
+    })
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
