d3c1580e44effbf12dc41fbebe3e6af320f0e2c9
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2022-01-19 22:11:20 +0700

    add procfile and port

--- app.js
+++ app.js
@@ -177,6 +177,12 @@ app.route("/events/:eventId/cancel/:userId")
         });
     })
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+
+if (port == null || port == ""){
+    port = 3000
+}
+
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
